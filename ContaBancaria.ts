815d030e259013da0ecbaf98ca727ace13a1fb67
Juliana Holanda <julianaholanda577@gmail.com>
2024-09-16 23:59:36 -0300

    primeiras linhas de código

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,14 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta: number
+    private agencia: number
+    private saldo: number = 0
+    private extrato: string[]
+    
+    constructor(){
+        this.numeroConta = 0
+        this.agencia = 1
+        this.saldo = 0
+        this.extrato = []
+
+    }
+}
