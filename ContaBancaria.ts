e0f588afbafd8ef86322165049f9749dce9f90ba
Juliana Holanda <julianaholanda577@gmail.com>
2024-09-17 21:33:58 -0300

    adicionando métodos

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -3,12 +3,27 @@ export default class ContaBancaria {
     private agencia: number
     private saldo: number = 0
     private extrato: string[]
-    
-    constructor(){
+
+    constructor() {
         this.numeroConta = 0
-        this.agencia = 1
+        this.agencia = 0
         this.saldo = 0
         this.extrato = []
 
     }
-}
+    depositar(valor: number): void {
+        if (valor > 0) {
+            this.saldo += valor;
+            this.registrarOperacao(`Depósito no valor de R$${valor}`);
+        } else {
+            console.log('Valor de depósito inválido.')
+        }
+    }
+    sacar(valor: number): void {
+        if (valor > 0 && valor <= this.saldo) {
+            this.saldo -= valor;
+        this.registrarOperacao(`'Saque realizado de R$${valor}`)
+        } else {
+            console.log('Saque inválido.')
+        }
+    }
